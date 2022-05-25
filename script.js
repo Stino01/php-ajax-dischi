950dc05b9dd92915b9cb0b59ba5f251fc22a1241
Stino01 <agostino.carrieri01@gmail.com>
2022-05-25 17:31:36 +0200

    bonus filter by select

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ var app = new Vue({
     data: {
       dischi: [],
       genres: [],
+      selectGenre: '',
       apiPath:'./server.php',
     },
     methods: {
-
-    },
-    mounted() {
-        axios.get(this.apiPath).then((res) => {
-            this.dischi = res.data;
+      search() {
+        this.getData(this.selectGenre);
+      },
+      getData(genere=null) {
+        let path;
+        if(genere) {
+          path = this.apiPath + '?genre=' + genere;
+        } else {
+          path = this.apiPath;
+        }
+        axios.get(path).then((res) => {
+          this.dischi = res.data;
+          if(this.genres.length < 1) {
             this.dischi.forEach((genr) => {
               if(!this.genres.includes(genr.genre)) {
                 this.genres.push(genr.genre);
               }
             });
+          }
         })
+      },
+    },
+    mounted() {
+        this.getData(this.selectGenre);
     }
   })
--- server.php
+++ server.php
@@ -1,6 +1,18 @@
 <?php
     include __DIR__ . '/database.php';
 
+    $result = [];
+
+    if(isset($_GET['genre'])) {
+        foreach($disc as $el) {
+            if($el['genre'] == $_GET['genre']) {
+                $result[] = $el;
+            }
+        }
+    } else {
+        $result = $disc;
+    }
+    
     header('Content-Type: application/json');
-    echo json_encode($disc);
+    echo json_encode($result);
 ?>
